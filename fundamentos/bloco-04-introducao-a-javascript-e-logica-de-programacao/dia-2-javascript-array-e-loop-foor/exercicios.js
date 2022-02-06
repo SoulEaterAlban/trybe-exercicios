b18c93a0a688d672799ad7a1ed968cca8310a9ac
Breno Vinicius <brenovinioli@gmail.com>
2022-02-06 15:19:38 -0300

    fiz todos os exercicios

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-foor/exercicios.js
@@ -0,0 +1,84 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+// Exercicio 1
+// for(index=0;index<numbers.length;index+=1){
+//     console.log(numbers[index]);
+// }
+
+// Exercicio 2
+// let soma =0;
+
+// for(index=0;index<numbers.length;index+=1){
+//     soma+=numbers[index];
+// }
+// console.log(soma);
+
+// Exercicio 3
+// let soma =0;
+
+// for(index=0;index<numbers.length;index+=1){
+//     soma+=numbers[index];
+// }
+// let aritmetica= soma/numbers.length;
+// console.log(aritmetica);
+
+// Exercicio 4
+// let soma =0;
+
+// for(index=0;index<numbers.length;index+=1){
+//     soma+=numbers[index];
+// }
+// let aritmetica= soma/numbers.length;
+// 
+// if (aritmetica>20){
+//     console.log("valor maior que 20")
+// }else{
+//     console.log("valor menor que 20");
+// }
+
+// Exercicio 4
+// let maximo =0;
+// for(index=0;index<numbers.length;index+=1){ 
+//     if(numbers[index]> maximo){
+//         maximo= numbers[index];
+//     }
+// }
+// console.log(maximo);
+
+// Exercicio 6
+// let contador =0;
+// for(index=0;index<numbers.length;index+=1){ 
+//     if(numbers[index]%2 != 0){
+//         contador +=1;
+//     }if(contador == 0){
+//         console.log("nenhum valor impar encontrado");
+//     }
+// }
+// console.log("Existem "+ contador + " numeros impares!");
+
+// Exercicio 7
+// let minimo = numbers[0];
+// for(index=1;index<=numbers.length;index+=1){ 
+//     if(numbers[index]< minimo){
+//         minimo= numbers[index];
+//     }
+// }
+// console.log(minimo);
+
+// Exercicio 8
+// let vetor= [];
+
+// for(index=1;index<26;index+=1){
+//     vetor.push(index);
+// }
+// console.log(vetor);
+
+// Exercicio 9
+// let vetor= [];
+// let divisao= 0;
+// for(index=1;index<26;index+=1){
+//     vetor.push(index);
+// }
+// for (index=0;index<vetor.length;index+=1){
+//     divisao=vetor[index]/2;
+//     console.log(divisao);
+// }
