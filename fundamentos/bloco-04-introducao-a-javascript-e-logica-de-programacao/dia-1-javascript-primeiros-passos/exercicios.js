9dbdfab3474e708fa52dab4232ec5fd2301e2a2c
Breno Vinicius <brenovinioli@gmail.com>
2022-02-06 12:39:00 -0300

    fiz alguns dos exercicios

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicios.js
@@ -0,0 +1,92 @@
+const a = 8;
+const b = 5;
+const c = 20;
+
+
+
+
+// exercicio 1
+// let c = a +b;
+// c = a - b;
+// c = a * b;
+// c = a / b;
+// c = a % b;
+// console.log(c);
+
+// exercicio 2
+// if(a > b){
+//     console.log(a)
+// }else if(b>a){
+//     console.log(b)
+// }else{
+//     console.log('São iguais')
+// }
+
+
+// exercicio 3
+// if(a > b && a > c){
+//     console.log(a)
+// }else if(b > a && b > c){
+//     console.log(b)
+// }else if(c > a && c > b){
+//     console.log(c)
+// }else{
+//     console.log('São iguais')
+// }
+
+//exercicio 4
+// if (a == 9){
+//     console.log('positive');
+// }else if (a != 9 || a == 0){
+//     console.log('negative');
+// }
+
+
+//exercicio 5
+// const x = 90;
+// const y = 30;
+// const z = 60;
+// if(x > 0 && y > 0 && z >0){
+//     if((x + y + z) == 180){
+//         console.log(true);
+//     }else{
+//         console.log(false);
+//     }
+
+// }else{
+//     console.log('erro angulo inválido')
+// }
+
+// Exercicio 6
+// let piece = "Rook"
+
+// switch (piece.toLowerCase()) {
+//     case "bishop":
+//         console.log("any squares vacants,diagonals")
+//         break;
+
+//     case "king":
+//         console.log("one square, all directions")
+//         break;
+
+//     case "queen":
+//         console.log("any squares vacants,all directions")
+//         break;
+
+//     case "pawn":
+//         console.log("only moves straight foward, capture pieces diagonnally")
+//         break;
+
+//     case "rook":
+//         console.log("any squares vacants,moves vertically and horizontally")
+//         break;
+
+//     case "knight":
+//         console.log("any squares vacants, moves in 'L' pattern ")
+//         break;
+
+//     default:
+//         console.log("No piece found!");
+//         break;
+// }
+
