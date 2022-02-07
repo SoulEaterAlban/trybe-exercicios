633c25f2632f42ba840e2476a93df3dc581c808e
Breno Vinicius <brenovinioli@gmail.com>
2022-02-07 20:10:03 -0300

    Terminei os outros exercicios]

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -33,3 +33,30 @@ let info = {
 //       console.log(info[key] + ' e ' + info2[key]);
 //     }
 //   }
+
+let leitor = {
+    nome: 'Julia',
+    sobrenome: 'Pessoa',
+    idade: 21,
+    livrosFavoritos: [
+      {
+        titulo: 'O Pior Dia de Todos',
+        autor: 'Daniela Kopsch',
+        editora: 'Tordesilhas',
+      },
+    ],
+  };
+ // Exercicio 7  
+  leitor.livrosFavoritos.push(
+    {
+        titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+        autor: 'JK Rowling',
+        editora: 'Rocco',
+      }
+  );
+// Exercicio 6
+//   console.log("O livro favorito de " + leitor.nome +" "+ leitor.sobrenome +" se chama " + leitor.livrosFavoritos[0].titulo);
+
+// Exercicio 8
+    console.log(leitor.nome + ' tem ' + leitor.livrosFavoritos.length + ' livros favoritos');
+
