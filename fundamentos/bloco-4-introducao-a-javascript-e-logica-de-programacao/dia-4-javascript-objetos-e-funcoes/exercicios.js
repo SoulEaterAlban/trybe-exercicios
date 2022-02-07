7083ea2cbfa8398d282a167964862e19a767b9cf
Breno Vinicius <brenovinioli@gmail.com>
2022-02-07 18:15:23 -0300

    Fiz os exercicios de 1 a 5

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -0,0 +1,35 @@
+let info = {
+    personagem: 'Margarida',
+    origem: 'Pato Donald',
+    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim',
+  };
+
+
+// Exercicio 1
+// console.log("Bem-vinda, "+info.personagem);
+
+// Exercicio 2
+// info['recorrente'] = 'Sim';
+// console.table(info);
+
+// Exercicio 3
+// for(let key in info){
+//     console.log(key);
+// }
+
+// Exercicio 5
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: 'Christmas on Bear Mountain, Dell’s Four Color Comics #178',
+//     nota: 'O último MacPatinhas',
+//     recorrente: 'Sim',
+//   };
+
+//   for (let key in info) {
+//     if (key == 'recorrente' && info[key] == 'Sim' && info2[key] == 'Sim') {
+//       console.log('Ambos recorrentes');
+//     } else {
+//       console.log(info[key] + ' e ' + info2[key]);
+//     }
+//   }
