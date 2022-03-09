4ec49d0d211cf362ec677fc3fa78667588d6e5bc
Breno Vinicius <brenovinioli@gmail.com>
2022-03-09 20:36:08 -0300

    Fiz a parte 1

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-1/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/parte-1/script.js
@@ -15,7 +15,6 @@ const testingScope = (escopo) => {
   const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
   // Seu código aqui.
-
-    // oddsAndEvens.sort(function (a, b) {return a - b});
+  // oddsAndEvens.sort(function (a, b) {return a - b});
 
   console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram ordenados de forma crescente !`); // será necessário alterar essa linha 😉
